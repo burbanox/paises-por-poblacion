1b4b159d7098617fb344b2a2b9e1e901c11e28ac
burbanox <haroldburbano2002@gmail.com>
2024-06-25 16:01:00 -0500

    Se programo el search

--- js/estados-unidos.js
+++ js/estados-unidos.js
@@ -1,4 +1,5 @@
 import { seleccionarPreguntasAleatorias } from "./preguntas.js";
+import {data} from "./paises.js"
 
 let carruselContenedor = document.getElementById("carrusel-quiz");
 let button = document.getElementById("button-quiz");
@@ -8,37 +9,44 @@ let portadaQuiz = document.getElementById("quiz-portada");
 let nextButton = document.getElementById("nextButton");
 let prevButton = document.getElementById("prevButton");
 let finishButton = document.getElementById("finishButton");
-let botonReiniciar = document.getElementById("boton-reiniciar")
-let portadaFinalizar = document.getElementById("portada-finalizar")
-let textoPuntajes = document.getElementById("puntos")
+let botonReiniciar = document.getElementById("boton-reiniciar");
+let portadaFinalizar = document.getElementById("portada-finalizar");
+let textoPuntajes = document.getElementById("puntos");
 
-const NAV_BUTTON = document.getElementById("nav-button")
-const NAV = document.getElementById("nav")
-const MENU = document.getElementById("menu-icono")
+const NAV_BUTTON = document.getElementById("nav-button");
+const NAV = document.getElementById("nav");
+const MENU = document.getElementById("menu-icono");
 
+const searchButton = document.getElementById("searchButton");
+const resultDiv = document.getElementById("result")
 
-MENU.addEventListener("click", ()=>
-{
-    NAV.classList.remove("desaparecer")
-})
-NAV_BUTTON.addEventListener("click", () =>
-{
-    NAV.classList.add("desaparecer")
-})
+MENU.addEventListener("click", () => {
+  NAV.classList.remove("desaparecer");
+});
+NAV_BUTTON.addEventListener("click", () => {
+  NAV.classList.add("desaparecer");
+});
 
+const SEARCH_BUTTON = document.getElementById("search-close-button");
+const SEARCH_CONTAINER = document.getElementById("search");
+const SEARCH_ICONO = document.getElementById("search-icono");
 
+SEARCH_ICONO.addEventListener("click", () => {
+  SEARCH_CONTAINER.classList.remove("desaparecer");
+});
+SEARCH_BUTTON.addEventListener("click", () => {
+  SEARCH_CONTAINER.classList.add("desaparecer");
+});
 
-
-let preguntaSeleccionadas = []
-
+let preguntaSeleccionadas = [];
 
 function logicaReiniciar() {
   portadaFinalizar.classList.add("desaparecer");
-  portadaQuiz.classList.remove("desaparecer")
-  button.classList.remove("desaparecer")
+  portadaQuiz.classList.remove("desaparecer");
+  button.classList.remove("desaparecer");
 }
 
-botonReiniciar.addEventListener("click",logicaReiniciar)
+botonReiniciar.addEventListener("click", logicaReiniciar);
 
 nextButton.addEventListener("click", function () {
   plusSlides(1);
@@ -52,80 +60,78 @@ finishButton.addEventListener("click", finishQuiz);
 
 let slideIndex = 1;
 
-function llenarPreguntas(preguntaSeleccionadas){
-  let enunciados = document.getElementsByClassName("quiz-title")
+function llenarPreguntas(preguntaSeleccionadas) {
+  let enunciados = document.getElementsByClassName("quiz-title");
 
-  for(let i=0;i<preguntaSeleccionadas.length;i++){
-    enunciados[i].textContent = preguntaSeleccionadas[i]['enunciado_pregunta']
+  for (let i = 0; i < preguntaSeleccionadas.length; i++) {
+    enunciados[i].textContent = preguntaSeleccionadas[i]["enunciado_pregunta"];
   }
 
-  for(let i=0;i<preguntaSeleccionadas.length;i++){
-    let opciones = document.getElementsByClassName("pregunta-" + i)
+  for (let i = 0; i < preguntaSeleccionadas.length; i++) {
+    let opciones = document.getElementsByClassName("pregunta-" + i);
 
-    for(let j=0;j<opciones.length;j++){
-      opciones[j].textContent = preguntaSeleccionadas[i]['opciones'][j]['textoOpcion']
+    for (let j = 0; j < opciones.length; j++) {
+      opciones[j].textContent =
+        preguntaSeleccionadas[i]["opciones"][j]["textoOpcion"];
     }
   }
-
-
 }
 
-function getSelecciones(){
-  let arr = []
+function getSelecciones() {
+  let arr = [];
 
-  for(let i =1;i<=5;i++){
-    let respuestas = []
+  for (let i = 1; i <= 5; i++) {
+    let respuestas = [];
     let radio = document.getElementsByName("pregunta_" + i);
-    
-    for(let j=0;j<4;j++){
-      console.log(radio)
-      respuestas.push(radio[j].checked)
+
+    for (let j = 0; j < 4; j++) {
+      console.log(radio);
+      respuestas.push(radio[j].checked);
     }
 
-    arr.push(respuestas)
+    arr.push(respuestas);
   }
 
-  return arr
+  return arr;
 }
 
+function puntaje(seleccionadas) {
+  let puntajeFinal = 0;
+  let diferentes = false;
 
-function puntaje(seleccionadas)
-{
-  let puntajeFinal = 0
-  let diferentes = false
-
-  for(let i=0;i<5;i++)
-    {
-      for(let j=0;j<4;j++)
-        {
-          if(!(preguntaSeleccionadas[i]["opciones"][j]["respuestaCorrecta"] == seleccionadas[i][j]))
-            {
-              diferentes = true
-              break
-            }
-        }
-
-        if(!diferentes){
-          puntajeFinal+=20
-        }
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (
+        !(
+          preguntaSeleccionadas[i]["opciones"][j]["respuestaCorrecta"] ==
+          seleccionadas[i][j]
+        )
+      ) {
+        diferentes = true;
+        break;
+      }
     }
-    return puntajeFinal
-}
 
+    if (!diferentes) {
+      puntajeFinal += 20;
+    }
+  }
+  return puntajeFinal;
+}
 
 function activarQuiz() {
   portadaQuiz.className += " desaparecer";
   carruselContenedor.classList.remove("desaparecer");
   button.className += " desaparecer";
-  preguntaSeleccionadas = seleccionarPreguntasAleatorias()
+  preguntaSeleccionadas = seleccionarPreguntasAleatorias();
 
-  $("input[type=radio]").prop('checked', false);
+  $("input[type=radio]").prop("checked", false);
 
-  slideIndex = 1
+  slideIndex = 1;
 
   showSlides(slideIndex);
 
-  llenarPreguntas(preguntaSeleccionadas)
+  llenarPreguntas(preguntaSeleccionadas);
 }
 
 button.addEventListener("click", activarQuiz);
@@ -189,18 +195,53 @@ function finishQuiz() {
   if (seleccionIncompleta) {
     alert(mensajeError);
   } else {
-
-    let selecciones = getSelecciones()
-    let puntos = puntaje(selecciones)
+    let selecciones = getSelecciones();
+    let puntos = puntaje(selecciones);
 
     carruselContenedor.classList.add("desaparecer");
-    portadaFinalizar.classList.remove("desaparecer")
+    portadaFinalizar.classList.remove("desaparecer");
 
-    textoPuntajes.textContent = puntos + "%"
+    textoPuntajes.textContent = puntos + "%";
 
-
-    console.log(puntos)
+    console.log(puntos);
   }
 }
 
 
+searchButton.addEventListener("click", function () {
+  const query = searchInput.value.toLowerCase();
+  performSearch(query);
+});
+
+function performSearch(query) {
+  let results = [];
+
+  // Buscar en continentes y países
+  for (let continent in data) {
+    if (continent.toLowerCase().includes(query)) {
+      results.push(`<strong>${continent}</strong>`);
+      results.push(
+        ...data[continent].map(
+          (country) => `<a href="${country.url}" id="etiqueta-pais">${country.nombre}</a>`
+        )
+      );
+    } else if (Array.isArray(data[continent])) {
+      const matchingCountries = data[continent].filter((country) =>
+        country.nombre.toLowerCase().includes(query)
+      );
+      if (matchingCountries.length > 0) {
+        results.push(`<strong>${continent}</strong>`);
+        results.push(
+          ...matchingCountries.map(
+            (country) => `<a href="${country.url}">${country.nombre}</a>`
+          )
+        );
+      }
+    }
+  }
+
+  // Mostrar los resultados
+  resultDiv.innerHTML = results.length
+    ? results.join("<br>")
+    : "No se encontraron resultados.";
+}
---
+++ js/paises.js
@@ -0,0 +1,115 @@
+let data = {
+    "Pagina principal" : "http://example.com/nigeria",
+    "Africa": [
+        {
+            "nombre": "Nigeria",
+            "url": "http://example.com/nigeria"
+        },
+        {
+            "nombre": "Etiopía",
+            "url": "http://example.com/etiopia"
+        },
+        {
+            "nombre": "Egipto",
+            "url": "http://example.com/egipto"
+        },
+        {
+            "nombre": "República Democrática del Congo",
+            "url": "http://example.com/rdc"
+        },
+        {
+            "nombre": "Tanzania",
+            "url": "http://example.com/tanzania"
+        }
+    ],
+    "Asia": [
+        {
+            "nombre": "China",
+            "url": "http://example.com/china"
+        },
+        {
+            "nombre": "India",
+            "url": "http://example.com/india"
+        },
+        {
+            "nombre": "Indonesia",
+            "url": "http://example.com/indonesia"
+        },
+        {
+            "nombre": "Pakistán",
+            "url": "http://example.com/pakistan"
+        },
+        {
+            "nombre": "Bangladesh",
+            "url": "http://example.com/bangladesh"
+        }
+    ],
+    "Europe": [
+        {
+            "nombre": "Rusia",
+            "url": "http://example.com/rusia"
+        },
+        {
+            "nombre": "Alemania",
+            "url": "http://example.com/alemania"
+        },
+        {
+            "nombre": "Reino Unido",
+            "url": "http://example.com/reino_unido"
+        },
+        {
+            "nombre": "Francia",
+            "url": "http://example.com/francia"
+        },
+        {
+            "nombre": "Italia",
+            "url": "http://example.com/italia"
+        }
+    ],
+    "America": [
+        {
+            "nombre": "Estados Unidos",
+            "url": "http://example.com/estados_unidos"
+        },
+        {
+            "nombre": "Brasil",
+            "url": "http://example.com/brasil"
+        },
+        {
+            "nombre": "México",
+            "url": "http://example.com/mexico"
+        },
+        {
+            "nombre": "Colombia",
+            "url": "http://example.com/colombia"
+        },
+        {
+            "nombre": "Argentina",
+            "url": "http://example.com/argentina"
+        }
+    ],
+    "Oceania": [
+        {
+            "nombre": "Australia",
+            "url": "http://example.com/australia"
+        },
+        {
+            "nombre": "Papúa Nueva Guinea",
+            "url": "http://example.com/papua_nueva_guinea"
+        },
+        {
+            "nombre": "Nueva Zelanda",
+            "url": "http://example.com/nueva_zelanda"
+        },
+        {
+            "nombre": "Fiyi",
+            "url": "http://example.com/fiyi"
+        },
+        {
+            "nombre": "Islas Salomón",
+            "url": "http://example.com/islas_salomon"
+        }
+    ]
+}
+
+export {data };
