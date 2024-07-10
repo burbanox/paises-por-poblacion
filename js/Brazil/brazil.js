82d7c5aaee04eb9de6429fb61dd9aa8f63df40f9
burbanox <haroldburbano2002@gmail.com>
2024-07-10 15:53:56 -0500

    Se termino Brazil

---
+++ js/Brazil/brazil.js
@@ -0,0 +1,255 @@
+import { data } from "../paises.js";
+import { seleccionarPreguntasAleatorias } from "./preguntas-brazil.js";
+
+const NAV_BUTTON = document.getElementById("nav-button");
+const NAV = document.getElementById("nav");
+const MENU = document.getElementById("menu-icono");
+
+const searchButton = document.getElementById("searchButton");
+const resultDiv = document.getElementById("result");
+const searchInput = document.getElementById("searchInput");
+
+MENU.addEventListener("click", () => {
+  NAV.classList.remove("desaparecer");
+});
+NAV_BUTTON.addEventListener("click", () => {
+  NAV.classList.add("desaparecer");
+});
+
+const SEARCH_BUTTON = document.getElementById("search-close-button");
+const SEARCH_CONTAINER = document.getElementById("search");
+const SEARCH_ICONO = document.getElementById("search-icono");
+
+SEARCH_ICONO.addEventListener("click", () => {
+  SEARCH_CONTAINER.classList.remove("desaparecer");
+});
+SEARCH_BUTTON.addEventListener("click", () => {
+  SEARCH_CONTAINER.classList.add("desaparecer");
+  resultDiv.classList.add("desaparecer");
+  searchInput.value = "";
+});
+
+searchButton.addEventListener("click", function () {
+  const query = searchInput.value.toLowerCase();
+  resultDiv.classList.remove("desaparecer");
+  performSearch(query);
+});
+
+function performSearch(query) {
+  let results = [];
+
+  // Buscar en continentes y países
+  for (let continent in data) {
+    if (continent.toLowerCase().includes(query)) {
+      results.push(`<strong>${continent}</strong>`);
+      results.push(
+        ...data[continent].map(
+          (country) =>
+            `<a href="${country.url}" id="etiqueta-pais">${country.nombre}</a>`
+        )
+      );
+    } else if (Array.isArray(data[continent])) {
+      const matchingCountries = data[continent].filter((country) =>
+        country.nombre.toLowerCase().includes(query)
+      );
+      if (matchingCountries.length > 0) {
+        results.push(`<strong>${continent}</strong>`);
+        results.push(
+          ...matchingCountries.map(
+            (country) => `<a href="${country.url}">${country.nombre}</a>`
+          )
+        );
+      }
+    }
+  }
+
+  // Mostrar los resultados
+  resultDiv.innerHTML = results.length
+    ? results.join("<br>")
+    : "No se encontraron resultados.";
+}
+
+
+let carruselContenedor = document.getElementById("carrusel-quiz");
+let button = document.getElementById("button-quiz");
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+let portadaQuiz = document.getElementById("quiz-portada");
+let nextButton = document.getElementById("nextButton");
+let prevButton = document.getElementById("prevButton");
+let finishButton = document.getElementById("finishButton");
+let botonReiniciar = document.getElementById("boton-reiniciar");
+let portadaFinalizar = document.getElementById("portada-finalizar");
+let textoPuntajes = document.getElementById("puntos");
+
+
+let preguntaSeleccionadas = [];
+
+function logicaReiniciar() {
+  portadaFinalizar.classList.add("desaparecer");
+  portadaQuiz.classList.remove("desaparecer");
+  button.classList.remove("desaparecer");
+}
+
+botonReiniciar.addEventListener("click", logicaReiniciar);
+
+nextButton.addEventListener("click", function () {
+  plusSlides(1);
+});
+
+prevButton.addEventListener("click", function () {
+  plusSlides(-1);
+});
+
+finishButton.addEventListener("click", finishQuiz);
+
+let slideIndex = 1;
+
+function llenarPreguntas(preguntaSeleccionadas) {
+  let enunciados = document.getElementsByClassName("quiz-title");
+
+  for (let i = 0; i < preguntaSeleccionadas.length; i++) {
+    enunciados[i].textContent = preguntaSeleccionadas[i]["enunciado_pregunta"];
+  }
+
+  for (let i = 0; i < preguntaSeleccionadas.length; i++) {
+    let opciones = document.getElementsByClassName("pregunta-" + i);
+
+    for (let j = 0; j < opciones.length; j++) {
+      opciones[j].textContent =
+        preguntaSeleccionadas[i]["opciones"][j]["textoOpcion"];
+    }
+  }
+}
+
+function getSelecciones() {
+  let arr = [];
+
+  for (let i = 1; i <= 5; i++) {
+    let respuestas = [];
+    let radio = document.getElementsByName("pregunta_" + i);
+
+    for (let j = 0; j < 4; j++) {
+      console.log(radio);
+      respuestas.push(radio[j].checked);
+    }
+
+    arr.push(respuestas);
+  }
+
+  return arr;
+}
+
+function puntaje(seleccionadas) {
+  let puntajeFinal = 0;
+  let diferentes = false;
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (
+        !(
+          preguntaSeleccionadas[i]["opciones"][j]["respuestaCorrecta"] ==
+          seleccionadas[i][j]
+        )
+      ) {
+        diferentes = true;
+        break;
+      }
+    }
+
+    if (!diferentes) {
+      puntajeFinal += 20;
+    }
+  }
+  return puntajeFinal;
+}
+
+function activarQuiz() {
+  portadaQuiz.classList.add("desaparecer");
+  carruselContenedor.classList.remove("desaparecer");
+  button.className += " desaparecer";
+  preguntaSeleccionadas = seleccionarPreguntasAleatorias();
+
+  $("input[type=radio]").prop("checked", false);
+
+  slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  llenarPreguntas(preguntaSeleccionadas);
+}
+
+button.addEventListener("click", activarQuiz);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  slides = document.getElementsByClassName("mySlides");
+  dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function verificarSeleccion(radios) {
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function finishQuiz() {
+  let forms = document.getElementsByClassName("contenedor-preguntas");
+  let mensajeError = "";
+  let seleccionIncompleta = false;
+
+  for (let i = 1; i <= forms.length; i++) {
+    let preguntas = document.getElementsByName("pregunta_" + i);
+
+    if (!verificarSeleccion(preguntas)) {
+      mensajeError +=
+        "Por favor selelcciona una opcion en la pregunta " + i + "\n";
+      seleccionIncompleta = true;
+    }
+  }
+
+  if (seleccionIncompleta) {
+    alert(mensajeError);
+  } else {
+    let selecciones = getSelecciones();
+    let puntos = puntaje(selecciones);
+
+    carruselContenedor.classList.add("desaparecer");
+    portadaFinalizar.classList.remove("desaparecer");
+
+    textoPuntajes.textContent = puntos + "%";
+
+    console.log(puntos);
+  }
+}
+
+
---
+++ js/Brazil/preguntas-brazil.js
@@ -0,0 +1,345 @@
+let preguntas = [
+  {
+    enunciado_pregunta: "¿Cuál es la capital de Brasil?",
+    opciones: [
+      { textoOpcion: "Río de Janeiro", respuestaCorrecta: false },
+      { textoOpcion: "Brasilia", respuestaCorrecta: true },
+      { textoOpcion: "São Paulo", respuestaCorrecta: false },
+      { textoOpcion: "Salvador", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Cuál es el idioma oficial de Brasil?",
+    opciones: [
+      { textoOpcion: "Español", respuestaCorrecta: false },
+      { textoOpcion: "Portugués", respuestaCorrecta: true },
+      { textoOpcion: "Inglés", respuestaCorrecta: false },
+      { textoOpcion: "Francés", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Cuál es el río más largo de Brasil?",
+    opciones: [
+      { textoOpcion: "Río Amazonas", respuestaCorrecta: true },
+      { textoOpcion: "Río Paraná", respuestaCorrecta: false },
+      { textoOpcion: "Río Tocantins", respuestaCorrecta: false },
+      { textoOpcion: "Río São Francisco", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el plato típico de Brasil conocido mundialmente?",
+    opciones: [
+      { textoOpcion: "Paella", respuestaCorrecta: false },
+      { textoOpcion: "Feijoada", respuestaCorrecta: true },
+      { textoOpcion: "Tacos", respuestaCorrecta: false },
+      { textoOpcion: "Ceviche", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Cuál es la selva tropical más grande de Brasil?",
+    opciones: [
+      { textoOpcion: "Selva Negra", respuestaCorrecta: false },
+      { textoOpcion: "Selva de Darién", respuestaCorrecta: false },
+      { textoOpcion: "Selva Amazónica", respuestaCorrecta: true },
+      { textoOpcion: "Selva Lacandona", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Cuál es el carnaval más famoso de Brasil?",
+    opciones: [
+      { textoOpcion: "Carnaval de Salvador", respuestaCorrecta: false },
+      { textoOpcion: "Carnaval de Recife", respuestaCorrecta: false },
+      { textoOpcion: "Carnaval de Río de Janeiro", respuestaCorrecta: true },
+      { textoOpcion: "Carnaval de Olinda", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Cuál es el principal deporte en Brasil?",
+    opciones: [
+      { textoOpcion: "Baloncesto", respuestaCorrecta: false },
+      { textoOpcion: "Voleibol", respuestaCorrecta: false },
+      { textoOpcion: "Fútbol", respuestaCorrecta: true },
+      { textoOpcion: "Tenis", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el nombre del famoso Cristo en Río de Janeiro?",
+    opciones: [
+      { textoOpcion: "Cristo de la Concordia", respuestaCorrecta: false },
+      { textoOpcion: "Cristo del Otero", respuestaCorrecta: false },
+      { textoOpcion: "Cristo Redentor", respuestaCorrecta: true },
+      { textoOpcion: "Cristo de las Noas", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Cuál es la moneda oficial de Brasil?",
+    opciones: [
+      { textoOpcion: "Dólar", respuestaCorrecta: false },
+      { textoOpcion: "Peso", respuestaCorrecta: false },
+      { textoOpcion: "Real", respuestaCorrecta: true },
+      { textoOpcion: "Euro", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el principal género musical originario de Brasil?",
+    opciones: [
+      { textoOpcion: "Tango", respuestaCorrecta: false },
+      { textoOpcion: "Samba", respuestaCorrecta: true },
+      { textoOpcion: "Merengue", respuestaCorrecta: false },
+      { textoOpcion: "Reguetón", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Qué país limita al sur con Brasil?",
+    opciones: [
+      { textoOpcion: "Colombia", respuestaCorrecta: false },
+      { textoOpcion: "Uruguay", respuestaCorrecta: true },
+      { textoOpcion: "Perú", respuestaCorrecta: false },
+      { textoOpcion: "Venezuela", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es la región más grande de Brasil en términos de área?",
+    opciones: [
+      { textoOpcion: "Región Nordeste", respuestaCorrecta: false },
+      { textoOpcion: "Región Centro-Oeste", respuestaCorrecta: false },
+      { textoOpcion: "Región Norte", respuestaCorrecta: true },
+      { textoOpcion: "Región Sur", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué recurso natural es Brasil uno de los principales exportadores?",
+    opciones: [
+      { textoOpcion: "Petróleo", respuestaCorrecta: false },
+      { textoOpcion: "Oro", respuestaCorrecta: false },
+      { textoOpcion: "Café", respuestaCorrecta: true },
+      { textoOpcion: "Diamantes", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el famoso festival de música brasileña que se celebra en Río de Janeiro?",
+    opciones: [
+      { textoOpcion: "Rock in Rio", respuestaCorrecta: true },
+      { textoOpcion: "Lollapalooza", respuestaCorrecta: false },
+      { textoOpcion: "Coachella", respuestaCorrecta: false },
+      { textoOpcion: "Tomorrowland", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué isla brasileña es famosa por sus playas paradisíacas?",
+    opciones: [
+      { textoOpcion: "Isla de Pascua", respuestaCorrecta: false },
+      { textoOpcion: "Fernando de Noronha", respuestaCorrecta: true },
+      { textoOpcion: "Galápagos", respuestaCorrecta: false },
+      { textoOpcion: "Isla Mujeres", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta: "¿Cuál es la ciudad más poblada de Brasil?",
+    opciones: [
+      { textoOpcion: "Río de Janeiro", respuestaCorrecta: false },
+      { textoOpcion: "São Paulo", respuestaCorrecta: true },
+      { textoOpcion: "Brasilia", respuestaCorrecta: false },
+      { textoOpcion: "Salvador", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el mayor productor de energía hidroeléctrica en Brasil?",
+    opciones: [
+      { textoOpcion: "Represa de Belo Monte", respuestaCorrecta: false },
+      { textoOpcion: "Represa de Itaipú", respuestaCorrecta: true },
+      { textoOpcion: "Represa de Jirau", respuestaCorrecta: false },
+      { textoOpcion: "Represa de Tucuruí", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el desierto que se encuentra en el noreste de Brasil?",
+    opciones: [
+      { textoOpcion: "Desierto de Atacama", respuestaCorrecta: false },
+      { textoOpcion: "Lençóis Maranhenses", respuestaCorrecta: true },
+      { textoOpcion: "Desierto de Sechura", respuestaCorrecta: false },
+      { textoOpcion: "Desierto del Sahara", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el famoso arquitecto brasileño conocido por diseñar muchos edificios en Brasilia?",
+    opciones: [
+      { textoOpcion: "Frank Lloyd Wright", respuestaCorrecta: false },
+      { textoOpcion: "Oscar Niemeyer", respuestaCorrecta: true },
+      { textoOpcion: "Le Corbusier", respuestaCorrecta: false },
+      { textoOpcion: "Ludwig Mies van der Rohe", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el nombre del famoso carnaval callejero en Salvador?",
+    opciones: [
+      { textoOpcion: "Carnaval de Olinda", respuestaCorrecta: false },
+      { textoOpcion: "Carnaval de Salvador", respuestaCorrecta: true },
+      { textoOpcion: "Carnaval de Recife", respuestaCorrecta: false },
+      { textoOpcion: "Carnaval de Río", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el nombre de la montaña en Río de Janeiro que ofrece vistas panorámicas de la ciudad?",
+    opciones: [
+      { textoOpcion: "Montaña de la Mesa", respuestaCorrecta: false },
+      { textoOpcion: "Montaña de las Ánimas", respuestaCorrecta: false },
+      { textoOpcion: "Pan de Azúcar", respuestaCorrecta: true },
+      { textoOpcion: "Montaña de Sal", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué famoso jugador de fútbol brasileño es conocido como 'O Rei'?",
+    opciones: [
+      { textoOpcion: "Zico", respuestaCorrecta: false },
+      { textoOpcion: "Romário", respuestaCorrecta: false },
+      { textoOpcion: "Pelé", respuestaCorrecta: true },
+      { textoOpcion: "Ronaldo", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué baile brasileño es conocido por su ritmo rápido y movimientos sensuales?",
+    opciones: [
+      { textoOpcion: "Samba", respuestaCorrecta: true },
+      { textoOpcion: "Tango", respuestaCorrecta: false },
+      { textoOpcion: "Salsa", respuestaCorrecta: false },
+      { textoOpcion: "Merengue", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué parque nacional en Brasil es conocido por su biodiversidad y grandes áreas de selva tropical?",
+    opciones: [
+      {
+        textoOpcion: "Parque Nacional de Yellowstone",
+        respuestaCorrecta: false,
+      },
+      {
+        textoOpcion: "Parque Nacional de los Lençóis Maranhenses",
+        respuestaCorrecta: false,
+      },
+      {
+        textoOpcion: "Parque Nacional de la Chapada Diamantina",
+        respuestaCorrecta: false,
+      },
+      { textoOpcion: "Parque Nacional del Iguazú", respuestaCorrecta: true },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué maracuyá es conocido en Brasil como un tranquilizante natural?",
+    opciones: [
+      { textoOpcion: "Maracuyá amarillo", respuestaCorrecta: true },
+      { textoOpcion: "Maracuyá morado", respuestaCorrecta: false },
+      { textoOpcion: "Maracuyá verde", respuestaCorrecta: false },
+      { textoOpcion: "Maracuyá rojo", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Cuál es el nombre de la estatua del Cristo Redentor en portugués?",
+    opciones: [
+      { textoOpcion: "Cristo Rei", respuestaCorrecta: false },
+      { textoOpcion: "Cristo Salvador", respuestaCorrecta: false },
+      { textoOpcion: "Cristo Redentor", respuestaCorrecta: true },
+      { textoOpcion: "Cristo de Corcovado", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué bebida alcohólica es típica de Brasil y se hace con cachaça, lima y azúcar?",
+    opciones: [
+      { textoOpcion: "Pisco Sour", respuestaCorrecta: false },
+      { textoOpcion: "Mojito", respuestaCorrecta: false },
+      { textoOpcion: "Caipirinha", respuestaCorrecta: true },
+      { textoOpcion: "Margarita", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué estado brasileño es conocido por sus pantanos y su abundante vida silvestre?",
+    opciones: [
+      { textoOpcion: "Mato Grosso", respuestaCorrecta: true },
+      { textoOpcion: "Amazonas", respuestaCorrecta: false },
+      { textoOpcion: "Bahía", respuestaCorrecta: false },
+      { textoOpcion: "Pernambuco", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué ciudad brasileña es conocida por su arquitectura colonial bien conservada?",
+    opciones: [
+      { textoOpcion: "Salvador", respuestaCorrecta: true },
+      { textoOpcion: "Río de Janeiro", respuestaCorrecta: false },
+      { textoOpcion: "São Paulo", respuestaCorrecta: false },
+      { textoOpcion: "Brasilia", respuestaCorrecta: false },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué famoso escritor brasileño escribió 'El Alquimista'?",
+    opciones: [
+      { textoOpcion: "Jorge Amado", respuestaCorrecta: false },
+      { textoOpcion: "Machado de Assis", respuestaCorrecta: false },
+      { textoOpcion: "Clarice Lispector", respuestaCorrecta: false },
+      { textoOpcion: "Paulo Coelho", respuestaCorrecta: true },
+    ],
+  },
+  {
+    enunciado_pregunta:
+      "¿Qué estado brasileño es famoso por sus playas y dunas de arena en la región noreste?",
+    opciones: [
+      { textoOpcion: "Pernambuco", respuestaCorrecta: false },
+      { textoOpcion: "Río Grande del Norte", respuestaCorrecta: true },
+      { textoOpcion: "Bahía", respuestaCorrecta: false },
+      { textoOpcion: "Ceará", respuestaCorrecta: false },
+    ],
+  },
+];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min); // Redondea hacia arriba para asegurarse de que se incluya el mínimo
+  max = Math.floor(max); // Redondea hacia abajo para asegurarse de que se incluya el máximo
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function seleccionarPreguntasAleatorias() {
+  let preguntaSeleccionadas = [];
+  let selecciones = [];
+
+  for (let i = 0; i < 5; i++) {
+    let indice = getRandomInt(0, preguntas.length - 1);
+    console.log(indice);
+
+    if (selecciones.includes(indice)) {
+      while (true) {
+        indice = getRandomInt(0, preguntas.length - 1);
+        if (!selecciones.includes(indice)) {
+          break;
+        }
+      }
+    }
+
+    selecciones.push(indice);
+
+    preguntaSeleccionadas.push(preguntas[indice]);
+  }
+
+  return preguntaSeleccionadas;
+}
+
+export { seleccionarPreguntasAleatorias };
