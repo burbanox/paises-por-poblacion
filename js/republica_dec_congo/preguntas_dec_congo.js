f8a07275fcee4c44579e9e7f24ac0357f45ef9ab
burbanox <haroldburbano2002@gmail.com>
2024-08-10 11:20:27 -0500

    Se termino Africa

---
+++ js/republica_dec_congo/preguntas_dec_congo.js
@@ -0,0 +1,291 @@
+let preguntas = [
+  {
+    enunciado_pregunta: "¿Cuál es la capital de la República Democrática del Congo?",
+    opciones: [
+      { textoOpcion: "Kisangani", respuestaCorrecta: false },
+      { textoOpcion: "Lubumbashi", respuestaCorrecta: false },
+      { textoOpcion: "Kinshasa", respuestaCorrecta: true },
+      { textoOpcion: "Goma", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué río es el más importante de la República Democrática del Congo?",
+    opciones: [
+      { textoOpcion: "Río Nilo", respuestaCorrecta: false },
+      { textoOpcion: "Río Congo", respuestaCorrecta: true },
+      { textoOpcion: "Río Zambezi", respuestaCorrecta: false },
+      { textoOpcion: "Río Níger", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué parque nacional en la RDC es conocido por su población de gorilas de montaña?",
+    opciones: [
+      { textoOpcion: "Parque Nacional de Kahuzi-Biega", respuestaCorrecta: false },
+      { textoOpcion: "Parque Nacional de Garamba", respuestaCorrecta: false },
+      { textoOpcion: "Parque Nacional de Virunga", respuestaCorrecta: true },
+      { textoOpcion: "Parque Nacional de Salonga", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué idioma es el oficial en la República Democrática del Congo?",
+    opciones: [
+      { textoOpcion: "Lingala", respuestaCorrecta: false },
+      { textoOpcion: "Swahili", respuestaCorrecta: false },
+      { textoOpcion: "Francés", respuestaCorrecta: true },
+      { textoOpcion: "Kikongo", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué recurso mineral es la RDC conocida por producir en grandes cantidades?",
+    opciones: [
+      { textoOpcion: "Oro", respuestaCorrecta: false },
+      { textoOpcion: "Cobalto", respuestaCorrecta: true },
+      { textoOpcion: "Hierro", respuestaCorrecta: false },
+      { textoOpcion: "Plata", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué importante lago se encuentra en la frontera entre la RDC y Ruanda?",
+    opciones: [
+      { textoOpcion: "Lago Victoria", respuestaCorrecta: false },
+      { textoOpcion: "Lago Tanganica", respuestaCorrecta: false },
+      { textoOpcion: "Lago Kivu", respuestaCorrecta: true },
+      { textoOpcion: "Lago Malawi", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Cuál es la ciudad más grande de la República Democrática del Congo?",
+    opciones: [
+      { textoOpcion: "Lubumbashi", respuestaCorrecta: false },
+      { textoOpcion: "Kinshasa", respuestaCorrecta: true },
+      { textoOpcion: "Kisangani", respuestaCorrecta: false },
+      { textoOpcion: "Goma", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué cadena montañosa se encuentra en la RDC y es hogar de los gorilas de montaña?",
+    opciones: [
+      { textoOpcion: "Montañas Rwenzori", respuestaCorrecta: false },
+      { textoOpcion: "Montes Atlas", respuestaCorrecta: false },
+      { textoOpcion: "Montañas Virunga", respuestaCorrecta: true },
+      { textoOpcion: "Montes Drakensberg", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué importante río africano fluye en la RDC y es conocido por su profundidad?",
+    opciones: [
+      { textoOpcion: "Río Níger", respuestaCorrecta: false },
+      { textoOpcion: "Río Congo", respuestaCorrecta: true },
+      { textoOpcion: "Río Limpopo", respuestaCorrecta: false },
+      { textoOpcion: "Río Zambezi", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué parque nacional en la RDC es el más antiguo de África?",
+    opciones: [
+      { textoOpcion: "Parque Nacional de Garamba", respuestaCorrecta: false },
+      { textoOpcion: "Parque Nacional de Virunga", respuestaCorrecta: true },
+      { textoOpcion: "Parque Nacional de Salonga", respuestaCorrecta: false },
+      { textoOpcion: "Parque Nacional de Kahuzi-Biega", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué grupo rebelde fue una parte significativa del conflicto en la RDC durante la Primera Guerra del Congo?",
+    opciones: [
+      { textoOpcion: "Al-Shabaab", respuestaCorrecta: false },
+      { textoOpcion: "Interahamwe", respuestaCorrecta: true },
+      { textoOpcion: "Boko Haram", respuestaCorrecta: false },
+      { textoOpcion: "LRA", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué recurso natural es abundante en la región de Katanga en la RDC?",
+    opciones: [
+      { textoOpcion: "Petróleo", respuestaCorrecta: false },
+      { textoOpcion: "Cobre", respuestaCorrecta: true },
+      { textoOpcion: "Diamantes", respuestaCorrecta: false },
+      { textoOpcion: "Gas natural", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué animal es símbolo de los esfuerzos de conservación en la RDC?",
+    opciones: [
+      { textoOpcion: "Elefante", respuestaCorrecta: false },
+      { textoOpcion: "Gorila de montaña", respuestaCorrecta: true },
+      { textoOpcion: "Leopardo", respuestaCorrecta: false },
+      { textoOpcion: "Rinoceronte negro", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué evento devastador tuvo lugar en la RDC en 1994 y afectó gravemente a la región?",
+    opciones: [
+      { textoOpcion: "Genocidio de Ruanda", respuestaCorrecta: true },
+      { textoOpcion: "Guerra Civil de Sierra Leona", respuestaCorrecta: false },
+      { textoOpcion: "Apartheid en Sudáfrica", respuestaCorrecta: false },
+      { textoOpcion: "Guerra de Angola", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué idioma es hablado ampliamente en la región oriental de la RDC?",
+    opciones: [
+      { textoOpcion: "Swahili", respuestaCorrecta: true },
+      { textoOpcion: "Lingala", respuestaCorrecta: false },
+      { textoOpcion: "Kikongo", respuestaCorrecta: false },
+      { textoOpcion: "Tshiluba", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué conflicto es conocido como la 'Gran Guerra de África' y tuvo lugar principalmente en la RDC?",
+    opciones: [
+      { textoOpcion: "Guerra del Congo", respuestaCorrecta: true },
+      { textoOpcion: "Guerra Civil de Ruanda", respuestaCorrecta: false },
+      { textoOpcion: "Guerra Civil de Sudán", respuestaCorrecta: false },
+      { textoOpcion: "Guerra de Angola", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué importante ciudad en la RDC es un centro de minería de diamantes?",
+    opciones: [
+      { textoOpcion: "Kisangani", respuestaCorrecta: false },
+      { textoOpcion: "Mbuji-Mayi", respuestaCorrecta: true },
+      { textoOpcion: "Goma", respuestaCorrecta: false },
+      { textoOpcion: "Kinshasa", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué tipo de ecosistema es común en la cuenca del Congo?",
+    opciones: [
+      { textoOpcion: "Desierto", respuestaCorrecta: false },
+      { textoOpcion: "Selva tropical", respuestaCorrecta: true },
+      { textoOpcion: "Sabana", respuestaCorrecta: false },
+      { textoOpcion: "Manglares", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué grupo étnico es mayoritario en la RDC?",
+    opciones: [
+      { textoOpcion: "Tutsi", respuestaCorrecta: false },
+      { textoOpcion: "Hutu", respuestaCorrecta: false },
+      { textoOpcion: "Luba", respuestaCorrecta: true },
+      { textoOpcion: "Zulu", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué región de la RDC es conocida por su producción de coltan, un mineral crucial para la tecnología?",
+    opciones: [
+      { textoOpcion: "Kivu", respuestaCorrecta: true },
+      { textoOpcion: "Katanga", respuestaCorrecta: false },
+      { textoOpcion: "Kasai", respuestaCorrecta: false },
+      { textoOpcion: "Bas-Congo", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué país vecino de la RDC es conocido por haber acogido a un gran número de refugiados durante el conflicto en la RDC?",
+    opciones: [
+      { textoOpcion: "Ruanda", respuestaCorrecta: true },
+      { textoOpcion: "Sudán del Sur", respuestaCorrecta: false },
+      { textoOpcion: "Tanzania", respuestaCorrecta: false },
+      { textoOpcion: "Angola", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué montaña en la RDC es conocida por ser uno de los volcanes más activos de África?",
+    opciones: [
+      { textoOpcion: "Monte Kilimanjaro", respuestaCorrecta: false },
+      { textoOpcion: "Monte Nyiragongo", respuestaCorrecta: true },
+      { textoOpcion: "Monte Rwenzori", respuestaCorrecta: false },
+      { textoOpcion: "Monte Meru", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué ciudad en la RDC es famosa por ser un centro de producción de cobre?",
+    opciones: [
+      { textoOpcion: "Goma", respuestaCorrecta: false },
+      { textoOpcion: "Lubumbashi", respuestaCorrecta: true },
+      { textoOpcion: "Bukavu", respuestaCorrecta: false },
+      { textoOpcion: "Mbandaka", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué tipo de gobierno tiene la República Democrática del Congo?",
+    opciones: [
+      { textoOpcion: "Monarquía", respuestaCorrecta: false },
+      { textoOpcion: "República", respuestaCorrecta: true },
+      { textoOpcion: "Dictadura", respuestaCorrecta: false },
+      { textoOpcion: "Oligarquía", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué importante parque nacional en la RDC es conocido por su biodiversidad y está amenazado por la actividad humana?",
+    opciones: [
+      { textoOpcion: "Parque Nacional de Garamba", respuestaCorrecta: false },
+      { textoOpcion: "Parque Nacional de Kahuzi-Biega", respuestaCorrecta: false },
+      { textoOpcion: "Parque Nacional de Virunga", respuestaCorrecta: true },
+      { textoOpcion: "Parque Nacional de Salonga", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué región en la RDC es famosa por sus plantaciones de café y su paisaje montañoso?",
+    opciones: [
+      { textoOpcion: "Kivu", respuestaCorrecta: true },
+      { textoOpcion: "Katanga", respuestaCorrecta: false },
+      { textoOpcion: "Kasai", respuestaCorrecta: false },
+      { textoOpcion: "Bas-Congo", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué famosa música congoleña es conocida mundialmente y se originó en la RDC?",
+    opciones: [
+      { textoOpcion: "Afrobeat", respuestaCorrecta: false },
+      { textoOpcion: "Rumba congoleña", respuestaCorrecta: true },
+      { textoOpcion: "Highlife", respuestaCorrecta: false },
+      { textoOpcion: "Makossa", respuestaCorrecta: false }
+    ]
+  },
+  {
+    enunciado_pregunta: "¿Qué explorador europeo es conocido por sus expediciones en la región del Congo?",
+    opciones: [
+      { textoOpcion: "David Livingstone", respuestaCorrecta: false },
+      { textoOpcion: "Henry Morton Stanley", respuestaCorrecta: true },
+      { textoOpcion: "Richard Burton", respuestaCorrecta: false },
+      { textoOpcion: "John Hanning Speke", respuestaCorrecta: false }
+    ]
+  }
+]
+
+  
+  
+  
+  function getRandomInt(min, max) {
+    min = Math.ceil(min); // Redondea hacia arriba para asegurarse de que se incluya el mínimo
+    max = Math.floor(max); // Redondea hacia abajo para asegurarse de que se incluya el máximo
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  function seleccionarPreguntasAleatorias() {
+    let preguntaSeleccionadas = [];
+    let selecciones = [];
+  
+    for (let i = 0; i < 5; i++) {
+      let indice = getRandomInt(0, preguntas.length - 1);
+      console.log(indice);
+  
+      if (selecciones.includes(indice)) {
+        while (true) {
+          indice = getRandomInt(0, preguntas.length - 1);
+          if (!selecciones.includes(indice)) {
+            break;
+          }
+        }
+      }
+  
+      selecciones.push(indice);
+  
+      preguntaSeleccionadas.push(preguntas[indice]);
+    }
+  
+    return preguntaSeleccionadas;
+  }
+  
+  export { seleccionarPreguntasAleatorias };
+  
---
+++ js/tanzania/preguntas-tanzania.js
@@ -0,0 +1,310 @@
+let preguntas = [
+    {
+      enunciado_pregunta: "¿Cuál es la capital oficial de Tanzania?",
+      opciones: [
+        { textoOpcion: "Dar es Salaam", respuestaCorrecta: false },
+        { textoOpcion: "Dodoma", respuestaCorrecta: true },
+        { textoOpcion: "Zanzíbar", respuestaCorrecta: false },
+        { textoOpcion: "Arusha", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué montaña es la más alta de África y se encuentra en Tanzania?",
+      opciones: [
+        { textoOpcion: "Monte Kilimanjaro", respuestaCorrecta: true },
+        { textoOpcion: "Monte Kenia", respuestaCorrecta: false },
+        { textoOpcion: "Monte Meru", respuestaCorrecta: false },
+        { textoOpcion: "Monte Elgon", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué parque nacional en Tanzania es famoso por la Gran Migración?",
+      opciones: [
+        { textoOpcion: "Parque Nacional de Tarangire", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional del Serengeti", respuestaCorrecta: true },
+        { textoOpcion: "Parque Nacional de Ruaha", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional de Mikumi", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Cuál es el idioma más hablado en Tanzania?",
+      opciones: [
+        { textoOpcion: "Inglés", respuestaCorrecta: false },
+        { textoOpcion: "Swahili", respuestaCorrecta: true },
+        { textoOpcion: "Árabe", respuestaCorrecta: false },
+        { textoOpcion: "Francés", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué famosa isla frente a la costa de Tanzania es conocida por sus playas y su historia?",
+      opciones: [
+        { textoOpcion: "Mafia", respuestaCorrecta: false },
+        { textoOpcion: "Zanzíbar", respuestaCorrecta: true },
+        { textoOpcion: "Pemba", respuestaCorrecta: false },
+        { textoOpcion: "Mnemba", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué parque nacional en Tanzania incluye la caldera volcánica intacta más grande del mundo?",
+      opciones: [
+        { textoOpcion: "Parque Nacional de Kilimanjaro", respuestaCorrecta: false },
+        { textoOpcion: "Área de Conservación de Ngorongoro", respuestaCorrecta: true },
+        { textoOpcion: "Parque Nacional de Serengeti", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional de Arusha", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué lago en Tanzania es uno de los más profundos del mundo?",
+      opciones: [
+        { textoOpcion: "Lago Victoria", respuestaCorrecta: false },
+        { textoOpcion: "Lago Tanganica", respuestaCorrecta: true },
+        { textoOpcion: "Lago Nyasa", respuestaCorrecta: false },
+        { textoOpcion: "Lago Manyara", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué lengua franca es utilizada por la mayoría de los tanzanos?",
+      opciones: [
+        { textoOpcion: "Inglés", respuestaCorrecta: false },
+        { textoOpcion: "Swahili", respuestaCorrecta: true },
+        { textoOpcion: "Árabe", respuestaCorrecta: false },
+        { textoOpcion: "Makonde", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué ciudad es el principal centro económico de Tanzania?",
+      opciones: [
+        { textoOpcion: "Dodoma", respuestaCorrecta: false },
+        { textoOpcion: "Arusha", respuestaCorrecta: false },
+        { textoOpcion: "Zanzíbar", respuestaCorrecta: false },
+        { textoOpcion: "Dar es Salaam", respuestaCorrecta: true }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué recurso natural es especialmente importante en la economía de Tanzania?",
+      opciones: [
+        { textoOpcion: "Diamantes", respuestaCorrecta: false },
+        { textoOpcion: "Gas natural", respuestaCorrecta: true },
+        { textoOpcion: "Oro", respuestaCorrecta: false },
+        { textoOpcion: "Carbón", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué región en Tanzania es famosa por sus plantaciones de especias?",
+      opciones: [
+        { textoOpcion: "Dodoma", respuestaCorrecta: false },
+        { textoOpcion: "Zanzíbar", respuestaCorrecta: true },
+        { textoOpcion: "Kigoma", respuestaCorrecta: false },
+        { textoOpcion: "Arusha", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué famoso sitio arqueológico en Tanzania ha proporcionado información clave sobre la evolución humana?",
+      opciones: [
+        { textoOpcion: "Garganta de Olduvai", respuestaCorrecta: true },
+        { textoOpcion: "Monte Kilimanjaro", respuestaCorrecta: false },
+        { textoOpcion: "Lago Manyara", respuestaCorrecta: false },
+        { textoOpcion: "Serengeti", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué grupo étnico es uno de los más numerosos en Tanzania?",
+      opciones: [
+        { textoOpcion: "Zulu", respuestaCorrecta: false },
+        { textoOpcion: "Hutu", respuestaCorrecta: false },
+        { textoOpcion: "Masái", respuestaCorrecta: true },
+        { textoOpcion: "Tutsi", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué famosa música originada en Tanzania ha influido en la música popular en África Oriental?",
+      opciones: [
+        { textoOpcion: "Benga", respuestaCorrecta: false },
+        { textoOpcion: "Taarab", respuestaCorrecta: true },
+        { textoOpcion: "Highlife", respuestaCorrecta: false },
+        { textoOpcion: "Afrobeat", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué grupo de islas frente a la costa de Tanzania es conocido por sus arrecifes de coral y playas?",
+      opciones: [
+        { textoOpcion: "Islas Maldivas", respuestaCorrecta: false },
+        { textoOpcion: "Zanzíbar", respuestaCorrecta: true },
+        { textoOpcion: "Islas Seychelles", respuestaCorrecta: false },
+        { textoOpcion: "Islas Comoras", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué país limita con Tanzania al norte?",
+      opciones: [
+        { textoOpcion: "Mozambique", respuestaCorrecta: false },
+        { textoOpcion: "Uganda", respuestaCorrecta: false },
+        { textoOpcion: "Kenia", respuestaCorrecta: true },
+        { textoOpcion: "Ruanda", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué ecosistema es común en el Parque Nacional de Ruaha en Tanzania?",
+      opciones: [
+        { textoOpcion: "Selva tropical", respuestaCorrecta: false },
+        { textoOpcion: "Sabana", respuestaCorrecta: true },
+        { textoOpcion: "Desierto", respuestaCorrecta: false },
+        { textoOpcion: "Manglares", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué importante recurso mineral se extrae en el norte de Tanzania?",
+      opciones: [
+        { textoOpcion: "Cobre", respuestaCorrecta: false },
+        { textoOpcion: "Tanzanita", respuestaCorrecta: true },
+        { textoOpcion: "Hierro", respuestaCorrecta: false },
+        { textoOpcion: "Plata", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué famosa área de conservación en Tanzania es conocida por ser el hábitat de los elefantes?",
+      opciones: [
+        { textoOpcion: "Parque Nacional de Serengeti", respuestaCorrecta: false },
+        { textoOpcion: "Área de Conservación de Ngorongoro", respuestaCorrecta: true },
+        { textoOpcion: "Parque Nacional de Ruaha", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional de Tarangire", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué lago en Tanzania es conocido por su alta alcalinidad y colores vibrantes?",
+      opciones: [
+        { textoOpcion: "Lago Victoria", respuestaCorrecta: false },
+        { textoOpcion: "Lago Manyara", respuestaCorrecta: false },
+        { textoOpcion: "Lago Natron", respuestaCorrecta: true },
+        { textoOpcion: "Lago Eyasi", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué lengua es considerada la madre del Swahili y se habla principalmente en Tanzania?",
+      opciones: [
+        { textoOpcion: "Makonde", respuestaCorrecta: false },
+        { textoOpcion: "Ngoni", respuestaCorrecta: false },
+        { textoOpcion: "Bantu", respuestaCorrecta: true },
+        { textoOpcion: "Haya", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué famoso parque nacional en Tanzania es conocido por ser el hogar de chimpancés?",
+      opciones: [
+        { textoOpcion: "Parque Nacional de Serengeti", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional de Gombe", respuestaCorrecta: true },
+        { textoOpcion: "Parque Nacional de Tarangire", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional de Mikumi", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué grupo étnico en Tanzania es conocido por sus características viviendas tradicionales de barro y paja?",
+      opciones: [
+        { textoOpcion: "Makonde", respuestaCorrecta: false },
+        { textoOpcion: "Masái", respuestaCorrecta: true },
+        { textoOpcion: "Sukuma", respuestaCorrecta: false },
+        { textoOpcion: "Chaga", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué famoso valle en Tanzania es conocido por su importancia arqueológica en la historia de la humanidad?",
+      opciones: [
+        { textoOpcion: "Valle del Rift", respuestaCorrecta: true },
+        { textoOpcion: "Valle de Tana", respuestaCorrecta: false },
+        { textoOpcion: "Valle de Nyika", respuestaCorrecta: false },
+        { textoOpcion: "Valle de Omo", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué tipo de gobierno tiene Tanzania?",
+      opciones: [
+        { textoOpcion: "Monarquía", respuestaCorrecta: false },
+        { textoOpcion: "República", respuestaCorrecta: true },
+        { textoOpcion: "Dictadura", respuestaCorrecta: false },
+        { textoOpcion: "Oligarquía", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué importante zona en Tanzania es conocida por su diversidad de vida silvestre y ha sido declarada Patrimonio de la Humanidad por la UNESCO?",
+      opciones: [
+        { textoOpcion: "Serengeti", respuestaCorrecta: true },
+        { textoOpcion: "Gombe", respuestaCorrecta: false },
+        { textoOpcion: "Tarangire", respuestaCorrecta: false },
+        { textoOpcion: "Selous", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué fenómeno natural atrae a millones de turistas a Tanzania cada año para presenciarlo en el Parque Nacional del Serengeti?",
+      opciones: [
+        { textoOpcion: "Erupción volcánica", respuestaCorrecta: false },
+        { textoOpcion: "La Gran Migración", respuestaCorrecta: true },
+        { textoOpcion: "Auroras boreales", respuestaCorrecta: false },
+        { textoOpcion: "Marea alta", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué animal es símbolo de conservación en Tanzania y se puede encontrar en el Parque Nacional de Serengeti?",
+      opciones: [
+        { textoOpcion: "León", respuestaCorrecta: true },
+        { textoOpcion: "Rinoceronte", respuestaCorrecta: false },
+        { textoOpcion: "Elefante", respuestaCorrecta: false },
+        { textoOpcion: "Jirafa", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué región en Tanzania es famosa por sus aguas termales naturales?",
+      opciones: [
+        { textoOpcion: "Mbeya", respuestaCorrecta: false },
+        { textoOpcion: "Arusha", respuestaCorrecta: true },
+        { textoOpcion: "Dodoma", respuestaCorrecta: false },
+        { textoOpcion: "Mwanza", respuestaCorrecta: false }
+      ]
+    },
+    {
+      enunciado_pregunta: "¿Qué característica geográfica importante forma la frontera occidental de Tanzania?",
+      opciones: [
+        { textoOpcion: "El Océano Índico", respuestaCorrecta: false },
+        { textoOpcion: "El Lago Tanganica", respuestaCorrecta: true },
+        { textoOpcion: "El Río Nilo", respuestaCorrecta: false },
+        { textoOpcion: "El Desierto de Kalahari", respuestaCorrecta: false }
+      ]
+    }
+  ]
+  
+  
+    
+    
+    
+    function getRandomInt(min, max) {
+      min = Math.ceil(min); // Redondea hacia arriba para asegurarse de que se incluya el mínimo
+      max = Math.floor(max); // Redondea hacia abajo para asegurarse de que se incluya el máximo
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    function seleccionarPreguntasAleatorias() {
+      let preguntaSeleccionadas = [];
+      let selecciones = [];
+    
+      for (let i = 0; i < 5; i++) {
+        let indice = getRandomInt(0, preguntas.length - 1);
+        console.log(indice);
+    
+        if (selecciones.includes(indice)) {
+          while (true) {
+            indice = getRandomInt(0, preguntas.length - 1);
+            if (!selecciones.includes(indice)) {
+              break;
+            }
+          }
+        }
+    
+        selecciones.push(indice);
+    
+        preguntaSeleccionadas.push(preguntas[indice]);
+      }
+    
+      return preguntaSeleccionadas;
+    }
+    
+    export { seleccionarPreguntasAleatorias };
+    
