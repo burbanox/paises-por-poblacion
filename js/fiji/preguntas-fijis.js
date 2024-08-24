e12bbf7fadffde4581993d7ab2eeb23dc6ee4071
burbanox <haroldburbano2002@gmail.com>
2024-08-24 10:59:40 -0500

    Se termino fiji y nueva zelanda

---
+++ js/fiji/preguntas-fijis.js
@@ -0,0 +1,301 @@
+let preguntas = [
+    {
+      enunciado_pregunta: "¿Cuál es la capital de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Auckland", respuestaCorrecta: false },
+        { textoOpcion: "Wellington", respuestaCorrecta: true },
+        { textoOpcion: "Christchurch", respuestaCorrecta: false },
+        { textoOpcion: "Dunedin", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué idioma indígena es oficial en Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Samoano", respuestaCorrecta: false },
+        { textoOpcion: "Maorí", respuestaCorrecta: true },
+        { textoOpcion: "Fiyi", respuestaCorrecta: false },
+        { textoOpcion: "Tongano", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué ciudad es conocida como la capital mundial de los deportes de aventura?",
+      opciones: [
+        { textoOpcion: "Auckland", respuestaCorrecta: false },
+        { textoOpcion: "Wellington", respuestaCorrecta: false },
+        { textoOpcion: "Queenstown", respuestaCorrecta: true },
+        { textoOpcion: "Rotorua", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué famoso director de cine es originario de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Steven Spielberg", respuestaCorrecta: false },
+        { textoOpcion: "Peter Jackson", respuestaCorrecta: true },
+        { textoOpcion: "James Cameron", respuestaCorrecta: false },
+        { textoOpcion: "Martin Scorsese", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cómo se llama el grupo indígena principal de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Inuit", respuestaCorrecta: false },
+        { textoOpcion: "Maorí", respuestaCorrecta: true },
+        { textoOpcion: "Aborígenes", respuestaCorrecta: false },
+        { textoOpcion: "Samoanos", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué deporte es el más popular en Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Fútbol", respuestaCorrecta: false },
+        { textoOpcion: "Rugby", respuestaCorrecta: true },
+        { textoOpcion: "Críquet", respuestaCorrecta: false },
+        { textoOpcion: "Netball", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué región de Nueva Zelanda es famosa por sus viñedos y producción de vino?",
+      opciones: [
+        { textoOpcion: "Marlborough", respuestaCorrecta: true },
+        { textoOpcion: "Bay of Plenty", respuestaCorrecta: false },
+        { textoOpcion: "Fiordland", respuestaCorrecta: false },
+        { textoOpcion: "Northland", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué cadena montañosa divide la Isla Sur de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Montañas Rocosas", respuestaCorrecta: false },
+        { textoOpcion: "Alpes del Sur", respuestaCorrecta: true },
+        { textoOpcion: "Montañas Azules", respuestaCorrecta: false },
+        { textoOpcion: "Andes", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es el pájaro nacional de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Kiwi", respuestaCorrecta: true },
+        { textoOpcion: "Kea", respuestaCorrecta: false },
+        { textoOpcion: "Tui", respuestaCorrecta: false },
+        { textoOpcion: "Moa", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué parque nacional de Nueva Zelanda es famoso por los fiordos Milford Sound y Doubtful Sound?",
+      opciones: [
+        { textoOpcion: "Parque Nacional Abel Tasman", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional Tongariro", respuestaCorrecta: false },
+        { textoOpcion: "Parque Nacional Fiordland", respuestaCorrecta: true },
+        { textoOpcion: "Parque Nacional Kahurangi", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué ciudad de Nueva Zelanda es conocida por su actividad geotérmica?",
+      opciones: [
+        { textoOpcion: "Auckland", respuestaCorrecta: false },
+        { textoOpcion: "Rotorua", respuestaCorrecta: true },
+        { textoOpcion: "Christchurch", respuestaCorrecta: false },
+        { textoOpcion: "Dunedin", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿En qué año se firmó el Tratado de Waitangi?",
+      opciones: [
+        { textoOpcion: "1780", respuestaCorrecta: false },
+        { textoOpcion: "1840", respuestaCorrecta: true },
+        { textoOpcion: "1900", respuestaCorrecta: false },
+        { textoOpcion: "1865", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué ciudad fue la primera capital de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Wellington", respuestaCorrecta: false },
+        { textoOpcion: "Russell", respuestaCorrecta: true },
+        { textoOpcion: "Auckland", respuestaCorrecta: false },
+        { textoOpcion: "Christchurch", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es la montaña más alta de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Monte Aspiring", respuestaCorrecta: false },
+        { textoOpcion: "Monte Ruapehu", respuestaCorrecta: false },
+        { textoOpcion: "Monte Cook", respuestaCorrecta: true },
+        { textoOpcion: "Monte Taranaki", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué especie de ave gigante extinta era nativa de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Dodo", respuestaCorrecta: false },
+        { textoOpcion: "Moa", respuestaCorrecta: true },
+        { textoOpcion: "Pájaros elefante", respuestaCorrecta: false },
+        { textoOpcion: "Avestruz", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué es el Haka?",
+      opciones: [
+        { textoOpcion: "Un plato tradicional", respuestaCorrecta: false },
+        { textoOpcion: "Una danza de guerra maorí", respuestaCorrecta: true },
+        { textoOpcion: "Un tipo de instrumento musical", respuestaCorrecta: false },
+        { textoOpcion: "Una celebración de año nuevo", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué deporte se juega en el estadio Eden Park en Auckland?",
+      opciones: [
+        { textoOpcion: "Críquet", respuestaCorrecta: false },
+        { textoOpcion: "Rugby", respuestaCorrecta: true },
+        { textoOpcion: "Fútbol", respuestaCorrecta: false },
+        { textoOpcion: "Hockey sobre césped", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué isla de Nueva Zelanda es conocida por sus paisajes volcánicos y geotérmicos?",
+      opciones: [
+        { textoOpcion: "Isla Sur", respuestaCorrecta: false },
+        { textoOpcion: "Isla Norte", respuestaCorrecta: true },
+        { textoOpcion: "Stewart Island", respuestaCorrecta: false },
+        { textoOpcion: "Isla Waiheke", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué famoso sendero en Nueva Zelanda se conoce como uno de los 'Great Walks'?",
+      opciones: [
+        { textoOpcion: "Routeburn Track", respuestaCorrecta: true },
+        { textoOpcion: "Appalachian Trail", respuestaCorrecta: false },
+        { textoOpcion: "Inca Trail", respuestaCorrecta: false },
+        { textoOpcion: "Pacific Crest Trail", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué cultura indígena es una parte integral de la identidad nacional de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Aborigen", respuestaCorrecta: false },
+        { textoOpcion: "Inuit", respuestaCorrecta: false },
+        { textoOpcion: "Maorí", respuestaCorrecta: true },
+        { textoOpcion: "Zulú", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué isla de Nueva Zelanda es famosa por su viticultura y playas?",
+      opciones: [
+        { textoOpcion: "Isla Norte", respuestaCorrecta: false },
+        { textoOpcion: "Stewart Island", respuestaCorrecta: false },
+        { textoOpcion: "Isla Waiheke", respuestaCorrecta: true },
+        { textoOpcion: "Isla Sur", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué rango ocupa Nueva Zelanda en términos de área terrestre a nivel mundial?",
+      opciones: [
+        { textoOpcion: "54", respuestaCorrecta: false },
+        { textoOpcion: "75", respuestaCorrecta: false },
+        { textoOpcion: "75", respuestaCorrecta: true },
+        { textoOpcion: "80", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es la ciudad más al sur de Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Wellington", respuestaCorrecta: false },
+        { textoOpcion: "Auckland", respuestaCorrecta: false },
+        { textoOpcion: "Invercargill", respuestaCorrecta: true },
+        { textoOpcion: "Dunedin", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué región de Nueva Zelanda es famosa por el rugby y los All Blacks?",
+      opciones: [
+        { textoOpcion: "Canterbury", respuestaCorrecta: true },
+        { textoOpcion: "Otago", respuestaCorrecta: false },
+        { textoOpcion: "Northland", respuestaCorrecta: false },
+        { textoOpcion: "Bay of Plenty", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué famosa trilogía de películas se filmó en gran parte en Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "Harry Potter", respuestaCorrecta: false },
+        { textoOpcion: "Star Wars", respuestaCorrecta: false },
+        { textoOpcion: "El Señor de los Anillos", respuestaCorrecta: true },
+        { textoOpcion: "Matrix", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué tipo de gobierno tiene Nueva Zelanda?",
+      opciones: [
+        { textoOpcion: "República", respuestaCorrecta: false },
+        { textoOpcion: "Monarquía constitucional", respuestaCorrecta: true },
+        { textoOpcion: "Dictadura", respuestaCorrecta: false },
+        { textoOpcion: "Anarquía", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué famosa formación geológica de Nueva Zelanda tiene columnas de basalto?",
+      opciones: [
+        { textoOpcion: "Waitomo Caves", respuestaCorrecta: false },
+        { textoOpcion: "Pancake Rocks", respuestaCorrecta: true },
+        { textoOpcion: "Fox Glacier", respuestaCorrecta: false },
+        { textoOpcion: "Moeraki Boulders", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué importante acuerdo entre los maoríes y la Corona Británica fue firmado en 1840?",
+      opciones: [
+        { textoOpcion: "Tratado de Versalles", respuestaCorrecta: false },
+        { textoOpcion: "Tratado de Waitangi", respuestaCorrecta: true },
+        { textoOpcion: "Declaración de Independencia", respuestaCorrecta: false },
+        { textoOpcion: "Carta Magna", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué volcán activo es un popular destino turístico en la Isla Norte?",
+      opciones: [
+        { textoOpcion: "Monte Egmont", respuestaCorrecta: false },
+        { textoOpcion: "Monte Tongariro", respuestaCorrecta: true },
+        { textoOpcion: "Monte Ruapehu", respuestaCorrecta: false },
+        { textoOpcion: "Monte Taranaki", respuestaCorrecta: false },
+      ],
+    }
+  ]
+  
+  
+    
+    
+    
+    function getRandomInt(min, max) {
+      min = Math.ceil(min); // Redondea hacia arriba para asegurarse de que se incluya el mínimo
+      max = Math.floor(max); // Redondea hacia abajo para asegurarse de que se incluya el máximo
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    function seleccionarPreguntasAleatorias() {
+      let preguntaSeleccionadas = [];
+      let selecciones = [];
+    
+      for (let i = 0; i < 5; i++) {
+        let indice = getRandomInt(0, preguntas.length - 1);
+        console.log(indice);
+    
+        if (selecciones.includes(indice)) {
+          while (true) {
+            indice = getRandomInt(0, preguntas.length - 1);
+            if (!selecciones.includes(indice)) {
+              break;
+            }
+          }
+        }
+    
+        selecciones.push(indice);
+    
+        preguntaSeleccionadas.push(preguntas[indice]);
+      }
+    
+      return preguntaSeleccionadas;
+    }
+    
+    export { seleccionarPreguntasAleatorias };
+    
